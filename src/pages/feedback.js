9dc5029e031f626d909b6dfc2b24a32f645f08d7
Josué Lobo <darksiders20005@gmail.com>
2021-09-08 15:08:17 -0300

    saifjid

---
+++ src/pages/feedback.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class FeedbackScreen extends Component {
+  render() {
+    return(
+      <h1>feedback</h1>
+    )
+  }
+}
+
+export default FeedbackScreen;
