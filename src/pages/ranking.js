ffe0b7b86f4b024618bd0906dcb573ec91543c19
Josué Lobo <darksiders20005@gmail.com>
2021-09-12 16:46:51 -0300

    resolvendo requisitos

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -1,62 +1,13 @@
 import React, { Component } from 'react';
-import md5 from 'crypto-js/md5';
 import { Link } from 'react-router-dom';
+import Header from '../componets/Header';
 
 class RankingScreen extends Component {
-  constructor() {
-    super();
-    this.state = {
-      ranking: [],
-    };
-    this.getInfo = this.getInfo.bind(this);
-    this.createGravatar = this.createGravatar.bind(this);
-    this.createLi = this.createLi.bind(this);
-  }
-
-  getInfo(key) {
-    const localGet = JSON.parse(localStorage.getItem('state'));
-    const { player } = localGet;
-    return player[key];
-  }
-
-  createGravatar() {
-    const gravatar = md5(this.getInfo('gravatarEmail').toString());
-    return gravatar;
-  }
-
-  rankStorage() {
-    const { name, score, picture } = JSON.parse(localStorage.getItem('ranking'));
-    const newEntries = { name, score, picture };
-    if (localStorage.ranking) {
-      const ranking = JSON.parse(localStorage.ranking);
-      const newRanking = [...ranking, newEntries];
-      newRanking.sort((a, b) => b.score - a.score);
-      JSON.stringfy(localStorage.setItem('ranking', newRanking));
-      this.setState({
-        ranking: newRanking,
-      });
-    } else {
-      const newRanking = [newEntries];
-      JSON.stringfy(localStorage.setItem('ranking', newRanking));
-      this.setState({
-        ranking: newRanking,
-      });
-    }
-  }
-
   render() {
-    const { ranking } = this.state;
     return (
       <div>
-        { ranking.map((player, indexPlayer) => (
-          <div
-            key={ indexPlayer }
-          >
-            <img src={ player.picture } alt="foto da pessoa" />
-            <h3 data-testid={ `player-name-${indexPlayer}` }>{ player.name }</h3>
-            <h3 data-testid={ `player-score-${indexPlayer}` }>{ player.score }</h3>
-          </div>
-        ))}
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <Header />
         <Link to="/">
           <button type="button" data-testid="btn-go-home">
             Tela Inicial
